902ad7d1c33a499dd7b983f67da355f67fbaff3f
Prashant Singh <149152581+prashantsingh8962@users.noreply.github.com>
2024-08-08 15:39:37 +0530

    Update Extractor.py

--- Backend/SRC/Extractor.py
+++ Backend/SRC/Extractor.py
@@ -1,34 +1,27 @@
-import pytesseract
 from pdf2image import convert_from_path
+import Util
+import pytesseract
+from parser_patient_details import PatientDetailsParser
+from parser_perscription import PerscriptionParser
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+Poppler_path = r"D:\Naveen\OCR_Healthcare_Project\poppler-22.04.0\Library\bin"
 
-# given path of poppler for support of pdf2image module, path to tesseract engine'exe'
-POPPLER_PATH = 'C:\\Users\\Prashant Singh\\OneDrive\\Documents\\Codebasics\\Python\\source-code\\4_project_medical_data_extraction\\poppler-24.07.0\\Library\\bin'
-pytesseract.pytesseract.tesseract_cmd = 'C:\\Users\\Prashant Singh\\OneDrive\\Documents\\Codebasics\\Python\\source-code\\4_project_medical_data_extraction\\Tesseract\\tesseract.exe'
-
-
-import Util  # Ensure this module is available and contains preprocess_image
-
-
-# Define paths to Poppler and Tesseract executables
-
-def extract(file_path, file_format):
-    # Extract text from PDF file
-    pages = convert_from_path(file_path, poppler_path=POPPLER_PATH)
-    document_text = ''
-
+def extract(file_path, file_type):
+    pages = convert_from_path(file_path, poppler_path=Poppler_path)
+    document_text = ""
     for page in pages:
         processed_image = Util.preprocess_image(page)
         text = pytesseract.image_to_string(processed_image, lang="eng")
-        document_text += text + '\n'  # Concatenate text from all pages
+        document_text += '\n' + text
 
-    return document_text
+    if file_type == "prescription":
+        extracted_data = PerscriptionParser(document_text).parse()
+    elif file_type == "patient_details":
+        extracted_data = PatientDetailsParser(document_text).parse()
+    else:
+        raise Exception(f"Invalid file type: {file_type}")
+    return extracted_data
 
-    # if file_format == 'prescription':
-    #     pass  # Extract data from prescription
-    # elif file_format == 'patient_details':
-    #     pass  # Extract data from patient_details
-    # ctrl +'/' is used to comment selected block in python
-
-if __name__ == '__main__':
-    data = extract('../Resource/prescription/pre_1.pdf', 'prescription')
-    print(data)
+if __name__ == "__main__":
+    pp = extract("../Resources/patient_details/pd_2.pdf", "patient_details")
+    print(pp)
